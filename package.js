a3e609d8e5951b092317aefa8e468e088939d241
Juan Sepulveda <juan@lavaina.com>
2014-09-19 17:58:32 -0400

    Bump version

--- package.js
+++ package.js
@@ -5,18 +5,18 @@ Npm.depends({
 Package.describe({
     summary: "The Chai Assertion Library, v1.8.1",
     name: "spacejamio:chai",
-    version: "0.1.6",
+    version: "1.0.0",
     git: "https://github.com/spacejamio/meteor-chai.git"
 });
 
-Package.on_use(function (api, where) {
-    api.add_files(['server.js'], ['server']);
-    api.add_files(['chai.js'], ['client']);
-    api.add_files(['exports.js'], ['client','server']);
+Package.onUse(function (api, where) {
+    api.addFiles(['server.js'], ['server']);
+    api.addFiles(['chai.js'], ['client']);
+    api.addFiles(['exports.js'], ['client','server']);
     api.export(['chai','assert','expect','should'],['client','server']);
 });
 
-Package.on_test(function(api) {
+Package.onTest(function(api) {
     api.use(['chai','tinytest', 'test-helpers',"templating","coffeescript"]);
-    api.add_files(['tests/chai.coffee']);
+    api.addFiles(['tests/chai.coffee']);
 });
